4943417d7b82e7ea1e0ebdd1bde9242c9db7f1a5
MD ALIF <alifkhan.bubt@gmail.com>
2024-01-18 18:29:15 +0600

    Hero, About, Courses, Reviews section is done but contact section has some isses.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Courses from './components/Courses'
+import Reviews from './components/Reviews'
+import Contact from './components/Contact'
 
 function App() {
   return (
     <>
       <Navbar />
-      
+      <main>
+        <div id='home'>
+          <Home />
+        </div>
+        <div id='about'>
+          <About />
+        </div>
+        <div id='courses'>
+          <Courses />
+        </div>
+        <div id='reviews'>
+          <Reviews />
+        </div>
+        <div id='contact'>
+          <Contact />
+        </div>
+
+
+      </main>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import { IoMdMenu } from "react-icons/io";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleChange = () => {
+    setMenu(!menu);
+  };
+
   return (
     <div>
       <div className="flex flex-row justify-between p-5 md:px-32 bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
@@ -10,23 +17,109 @@ const Navbar = () => {
             eStudy
           </Link>
         </div>
-        <nav className="flex gap-5 font-medium p-1 cursor-pointer">
-          <Link to="home" spy={true} smooth={true} duration={500} className="hover: text-green transition-all">
+        <nav className="hidden md:flex gap-5 font-medium p-1 cursor-pointer">
+          <Link
+            to="home"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-green-500  transition-all"
+          >
             Home
           </Link>
-          <Link to="about" spy={true} smooth={true} duration={500} className="hover: text-green transition-all">
+          <Link
+            to="about"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-green-500  transition-all"
+          >
             About
           </Link>
-          <Link to="courses" spy={true} smooth={true} duration={500} className="hover: text-green transition-all">
+          <Link
+            to="courses"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-green-500  transition-all"
+          >
             Courses
           </Link>
-          <Link to="Reviews" spy={true} smooth={true} duration={500} className="hover: text-green transition-all">
+          <Link
+            to="Reviews"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-green-500  transition-all"
+          >
             reviews
           </Link>
-          <Link to="contact" spy={true} smooth={true} duration={500} className="hover: text-green transition-all">
+          <Link
+            to="contact"
+            spy={true}
+            smooth={true}
+            duration={500}
+            className="hover:text-green-500  transition-all"
+          >
             Contact
           </Link>
         </nav>
+        <div className="flex md:hidden" onClick={handleChange}>
+          <div className="p-2">
+            <IoMdMenu size={22} />
+          </div>
+        </div>
+      </div>
+      <div
+        className={`${
+          menu ? "translate-x-0" : "-translate-x-full"
+        } md:hidden flex flex-col absolute bg-white left-0 top-20 font-medium text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300`}
+      >
+        <Link
+          to="home"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-green-500  marker:transition-all"
+        >
+          Home
+        </Link>
+        <Link
+          to="about"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-green-500  transition-all"
+        >
+          About
+        </Link>
+        <Link
+          to="courses"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-green-500  transition-all"
+        >
+          Courses
+        </Link>
+        <Link
+          to="Reviews"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-green-500  transition-all"
+        >
+          reviews
+        </Link>
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="hover:text-green-500  transition-all"
+        >
+          Contact
+        </Link>
       </div>
     </div>
   );
